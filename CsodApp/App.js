aa2dd89e67ad6677c837dc625a8b81dfaf223b02
gyorgynovak <gnovak@loginet.hu>
2022-02-22 23:54:44 +0100

    CSODAPP-13 add title and authors

--- CsodApp/App.js
+++ CsodApp/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
 
 export default function App() {
+  const component = (
+    <Text style={styles.subTitle}>developed by CsodApp team</Text>
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>CsodApp</Text>
       <StatusBar style="auto" />
-    </View>
+      {component}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 40,
+    color: "#74bcc4",
   },
 });
