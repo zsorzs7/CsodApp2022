7d3490a97dd5e778ff29fb814a2c920db13d41b4
zsorzs7 <maneqvin@gmail.com>
2022-02-27 10:55:28 +0100

    CSODAPP-13 firebase starter used for creating project

--- CsodApp/App.js
+++ CsodApp/App.js
@@ -1,60 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import CsodAppFooter from "./Components/CsodAppFooter";
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-function HomeScreen({ navigation }) {
+import { RootNavigator } from './navigation/RootNavigator';
+import { AuthenticatedUserProvider } from './providers';
+
+const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-      <Button title="Media" onPress={() => navigation.navigate("Media")} />
-      <CsodAppFooter></CsodAppFooter>
-    </View>
+    <AuthenticatedUserProvider>
+      <SafeAreaProvider>
+        <RootNavigator />
+      </SafeAreaProvider>
+    </AuthenticatedUserProvider>
   );
-}
+};
 
-function MediaScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Media</Text>
-      <Button title="Home" onPress={() => navigation.navigate("Home")} />
-      <CsodAppFooter></CsodAppFooter>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Media"
-          options={{ headerShown: false }}
-          component={MediaScreen}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{ headerShown: false }}
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    paddingTop: 60,
-  },
-  title: {
-    fontSize: 40,
-    color: "#74bcc4",
-  },
-});
+export default App;
