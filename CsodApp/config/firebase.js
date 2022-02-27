8b24f0e7affe4f828684f24fc71fffbec8de5d89
zsorzs7 <maneqvin@gmail.com>
2022-02-27 11:28:41 +0100

    CSODAPP-13 fetch titles items

--- CsodApp/config/firebase.js
+++ CsodApp/config/firebase.js
@@ -1,15 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import Constants from 'expo-constants';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import Constants from "expo-constants";
 
 // add firebase config
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId
+  apiKey: "AIzaSyDlK1kLJ5zp-z9JnIpi7wRxVxBFg-I19pY",
+  authDomain: "csodapp-2022.firebaseapp.com",
+  projectId: "csodapp-2022",
+  storageBucket: "csodapp-2022.appspot.com",
+  messagingSenderId: "679442946290",
+  appId: "1:679442946290:web:f938a9345ecc0346ad586b",
+  measurementId: "G-QDH2NQGFWT",
 };
 
 // initialize firebase
--- CsodApp/screens/HomeScreen.js
+++ CsodApp/screens/HomeScreen.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import { signOut } from 'firebase/auth';
-
-import { auth } from '../config';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Button, Text } from "react-native";
+import { signOut } from "firebase/auth";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { auth } from "../config";
 
 export const HomeScreen = () => {
+  const [titles, setTitles] = useState(0);
+
+  const getTitles = async () => {
+    const db = getFirestore();
+    const table = collection(db, "media");
+    const titleCollection = await getDocs(table);
+    const titles = titleCollection.docs.map((doc) => doc.data());
+    setTitles(titles);
+  };
+
+  useEffect(() => {
+    getTitles();
+  }, []);
+
   const handleLogout = () => {
-    signOut(auth).catch(error => console.log('Error logging out: ', error));
+    signOut(auth).catch((error) => console.log("Error logging out: ", error));
   };
   return (
     <View style={styles.container}>
-      <Button title='Sign Out' onPress={handleLogout} />
+      <Button title="Sign Out" onPress={async () => getTitles()} />
+      <Text>{JSON.stringify(titles)}</Text>
+      <Button title="Bigyo" onPress={async () => setTitles(12)} />
+      <Text>CsodApp 2022</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
